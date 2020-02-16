afa3a2f3a68cb939f4972b7ce19a98d11bc2a0ff
vsed <vsed>
2020-02-16 03:03:25 +0100

    upd

--- db-conf.sh
+++ db-conf.sh
@@ -6,6 +6,5 @@ echo 'bind-address = 0.0.0.0' >> /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mysql
 
 mysql -e "CREATE DATABASE counter"
-mysql -e "CREATE TABLE counter(hostname VARCHAR(30), count INT)"
-mysql -e "INSERT INTO counter(hostname, count) VALUES ('vm1', 0)"
+mysql -Dcounter -e "CREATE TABLE counter(hostname VARCHAR(30), count INT)"
 mysql -e "GRANT ALL ON counter.* TO counter@'10.0.0.%' IDENTIFIED BY 'counter'"
