4b74972a7f598a15a7766562313d1ce26681a706
vsed <vsed>
2020-02-17 21:22:00 +0100

    upd

--- node-setup.sh
+++ node-setup.sh
@@ -4,9 +4,9 @@ apt -y install apache2 php php-mysql mariadb-client nmap >> /var/log/mujlog
 
 rm /var/www/html/index.html
 
-echo '#!/bin/bash
-echo $(hostname)
-' > /usr/lib/cgi-bin/hostname.sh
+echo "#!/bin/bash
+echo \"'$(hostname)'\"
+" > /usr/lib/cgi-bin/hostname.sh
 chmod +x /usr/lib/cgi-bin/hostname.sh
 
 systemctl restart apache2
@@ -17,7 +17,7 @@ mysql -ucounter -pcounter -Dcounter -h $db -e "INSERT INTO counter(hostname, cou
 
 echo '<?php
 $hostname = shell_exec("/usr/lib/cgi-bin/hostname.sh");
-echo "<pre>$hostname</pre>";' > /var/www/html/index.php
+echo "<pre>Server: $hostname</pre>";' > /var/www/html/index.php
 echo \$servername = \""$db"\"\; >> /var/www/html/index.php
 echo '$username = "counter";
 $password = "counter";
@@ -44,16 +44,15 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 
-$sqlr = "SELECT count FROM counter WHERE hostname='$hostname'";
+$sqlr = "SELECT count FROM counter WHERE hostname='"$hostname"';
 $rres = $conn->query($sqlr); 
 $data = $rres->fetch_assoc();
-$cnt = $data['count'];
-echo $cnt;
+$cnt = $data['"'count'"'];
 $cnt +=1;
-$sqlc = "UPDATE counter SET count=$cnt WHERE hostname='$hostname'";
+$sqlc = "UPDATE counter SET count=$cnt WHERE hostname='"$hostname"';
 
 if ($conn->query($sqlc) === TRUE) {
-    echo "Record updated successfully";
+    echo "<br>";
 } else {
     echo "Error updating record: " . $conn->error;
 }
