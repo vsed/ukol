e8c5df3a1321b354abae9f9c2d063d0b9c2be3b5
vsed <vsed>
2020-02-17 20:11:50 +0100

    upd

--- node-setup.sh
+++ node-setup.sh
@@ -38,7 +38,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["hostname"]. " - Value: " . $row["count"] . "<br>";
+        echo "Server: " . $row["hostname"]. " - Pristupy: " . $row["count"] . "<br>";
     }
 } else {
     echo "0 results";
