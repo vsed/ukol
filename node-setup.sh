2e30d843ef3d29d3e6d65bcd8973dafc58a4350c
vsed <vsed>
2020-02-18 15:46:52 +0100

    upd

--- node-setup.sh
+++ node-setup.sh
@@ -34,7 +34,7 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM counter";
 $result = $conn->query($sql);
-// echo $result;
+echo "Vypis databaze s pristupy:<br>";
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
